52729e027707a058fc205ccc6feb77d884785115
Arunam Gupta <arunamgupta97@gmail.com>
2021-09-23 01:04:06 -0400

    MAJOR CHANGES.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,23 @@
-/* alert("Hello There!") */
+function openSection(input) {
+    if (input === "aboutme") {
+        document.getElementById("aboutme_body").style.display = "block";
+        document.getElementById("education_body").style.display = "none";
+        document.getElementById("skills_body").style.display = "none";
+        document.getElementById("projects_body").style.display = "none";
+    } else if (input === "education") {
+        document.getElementById("aboutme_body").style.display = "none";
+        document.getElementById("education_body").style.display = "block";
+        document.getElementById("skills_body").style.display = "none";
+        document.getElementById("projects_body").style.display = "none";
+    } else if (input === "skills") {
+        document.getElementById("aboutme_body").style.display = "none";
+        document.getElementById("education_body").style.display = "none";
+        document.getElementById("skills_body").style.display = "block";
+        document.getElementById("projects_body").style.display = "none";
+    } else if (input === "projects") {
+        document.getElementById("aboutme_body").style.display = "none";
+        document.getElementById("education_body").style.display = "none";
+        document.getElementById("skills_body").style.display = "none";
+        document.getElementById("projects_body").style.display = "block";
+    }
+}
