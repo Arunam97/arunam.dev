f75d1bc4d038d98f13d67f60bd2a64a809705dcd
Arunam Gupta <arunamgupta97@gmail.com>
2021-09-23 01:50:52 -0400

    changes after review

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,40 @@
 function openSection(input) {
     if (input === "aboutme") {
+        document.getElementById("aboutme_btn").style.fontSize = "25px";
+        document.getElementById("education_btn").style.fontSize = "15px";
+        document.getElementById("skills_btn").style.fontSize = "15px";
+        document.getElementById("projects_btn").style.fontSize = "15px";
+
         document.getElementById("aboutme_body").style.display = "block";
         document.getElementById("education_body").style.display = "none";
         document.getElementById("skills_body").style.display = "none";
         document.getElementById("projects_body").style.display = "none";
     } else if (input === "education") {
+        document.getElementById("aboutme_btn").style.fontSize = "15px";
+        document.getElementById("education_btn").style.fontSize = "25px";
+        document.getElementById("skills_btn").style.fontSize = "15px";
+        document.getElementById("projects_btn").style.fontSize = "15px";
+
         document.getElementById("aboutme_body").style.display = "none";
         document.getElementById("education_body").style.display = "block";
         document.getElementById("skills_body").style.display = "none";
         document.getElementById("projects_body").style.display = "none";
     } else if (input === "skills") {
+        document.getElementById("aboutme_btn").style.fontSize = "15px";
+        document.getElementById("education_btn").style.fontSize = "15px";
+        document.getElementById("skills_btn").style.fontSize = "25px";
+        document.getElementById("projects_btn").style.fontSize = "15px";
+
         document.getElementById("aboutme_body").style.display = "none";
         document.getElementById("education_body").style.display = "none";
         document.getElementById("skills_body").style.display = "block";
         document.getElementById("projects_body").style.display = "none";
     } else if (input === "projects") {
+        document.getElementById("aboutme_btn").style.fontSize = "15px";
+        document.getElementById("education_btn").style.fontSize = "15px";
+        document.getElementById("skills_btn").style.fontSize = "15px";
+        document.getElementById("projects_btn").style.fontSize = "25px";
+
         document.getElementById("aboutme_body").style.display = "none";
         document.getElementById("education_body").style.display = "none";
         document.getElementById("skills_body").style.display = "none";
